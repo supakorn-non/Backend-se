798427b8e8ca9e8f988add186cf9827f3ef33a70
supakorn <supakorn.tank@ku.th>
2021-04-16 16:40:49 +0700

    first

---
+++ src/test/java/th/ac/ku/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
